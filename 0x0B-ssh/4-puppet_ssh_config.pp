c8ab4e27623f8c6a66cd08a799a287e0a4fcea13
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-05 05:44:23 +0000

    first attempt t4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# DevOps 0x0B task 4. Client configuration file (w/ Puppet)
+# disable password ssh authentication
+file_line { 'Turn off passwd auth':
+  ensure => file,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+# name 'holberton' identity (private key) file
+file_line { 'Declare identity file':
+  ensure => file,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton
+',
+}
