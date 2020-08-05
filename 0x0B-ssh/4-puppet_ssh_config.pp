75ee4ff89fb4b4c4fe464573974741b1f7bb0c9c
Samuel Pomeroy <1690@holbertonschool.com>
2020-08-05 06:07:52 +0000

    `file_line` only allows for `file` values `present`, `absent`

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # DevOps 0x0B task 4. Client configuration file (w/ Puppet)
 # disable password ssh authentication
 file_line { 'Turn off passwd auth':
-  ensure => file,
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 # name 'holberton' identity (private key) file
 file_line { 'Declare identity file':
-  ensure => file,
+  ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
