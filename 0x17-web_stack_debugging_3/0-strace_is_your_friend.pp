82843c05c2159ad55938bef6fc1e6330654fff68
Samuel Pomeroy <1690@holbertonschool.com>
2020-09-29 23:34:51 +0000

    first attempt t0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+file { '/var/www/html/wp-settings.php' :
+  ensure  => file,
+}
+-> exec { 'replace reference to /class-wp-locale.phpp':
+  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s|/class-wp-locale.phpp|/class-wp-locale.php|" /var/www/html/wp-settings.php'\
+,
+}
